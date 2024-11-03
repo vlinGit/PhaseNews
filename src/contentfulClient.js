01b4ae415a292cd93d94c3d3192cface50ac091a
Vincent Lin <vincent@pockyt.io>
2024-11-03 16:15:21 -0500

    contentful

--- src/contentfulClient.js
+++ src/contentfulClient.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful'
 
 export const client = contentful.createClient({
-    space: 'dn46jplu500m',
-    accessToken: '-UIjdqqXvbCJLbIFdF31C-QaB6674tGlfLZyRzi9Nlk'
+    space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.VITE_CONTENTFUL_API_TOKEN
 })
--- src/views/home.vue
+++ src/views/home.vue
@@ -7,7 +7,10 @@ import { onMounted } from 'vue'
 import { client } from '@/contentfulClient.js'
 
 onMounted(() => {
-
+    client.getEntries()
+        .then((response) => {
+            console.log(response.items)
+        }).catch(console.error)
 })
 </script>
 
