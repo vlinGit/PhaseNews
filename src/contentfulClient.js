61019f2202baa9b9a314ed0ec782f77fa0a89c63
Vincent Lin <vincent@pockyt.io>
2024-11-03 16:06:32 -0500

    contentful

---
+++ src/contentfulClient.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful'
+
+export const client = contentful.createClient({
+    space: 'dn46jplu500m',
+    accessToken: '-UIjdqqXvbCJLbIFdF31C-QaB6674tGlfLZyRzi9Nlk'
+})
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>hello</p>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { client } from '@/contentfulClient.js'
+
+onMounted(() => {
+
+})
+</script>
+
+<style scoped>
+
+</style>
