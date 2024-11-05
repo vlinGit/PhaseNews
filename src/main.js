605c98b8113fec9cb46d9ad8235c4837654b9b09
Vincent Lin <vincent@pockyt.io>
2024-11-05 18:11:15 -0500

    analytics

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice';
 import Toast from 'primevue/toast';
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(router)
 app.use(PrimeVue);
 app.use(ToastService);
+app.use(VueGtag, {
+    config: { id: "G-6FHDG5L3H4"}
+})
 app.component('Toast', Toast);
 
 app.mount('#app')
